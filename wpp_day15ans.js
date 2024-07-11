0cb5d23e88e3441f1ad1922e1f2917f924d745f0
sweetypradhan <sweetypradhan0000@gmail.com>
2024-07-11 20:29:28 +0530

    wrote a code to compute the sum of three element

---
+++ wpp_day15ans.js
@@ -0,0 +1,9 @@
+//Write a JavaScript program to compute the sum of three elements of a given array of integers of length 3
+
+
+function sum_of_three(nums){
+    return nums[0] + nums[1] + nums[2];
+}
+
+console.log(sum_of_three([10,20,30]));
+console.log(sum_of_three([5,10,20]));
